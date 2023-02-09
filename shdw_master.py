3dfa0310a3cb664bfaad50065e396aba99e7c619
Jay <83360782+Js0nGIT@users.noreply.github.com>
2023-02-09 11:51:20 +0000

    Update titles

--- shdw_master.py
+++ shdw_master.py
@@ -12,23 +12,33 @@ from colorama import Fore
 colorama.init(autoreset=False)
 
 if platform.system() != "Windows":
+    ctypes.windll.kernel32.SetConsoleTitleW(
+        f"ERROR: Your OS ({platform.system()}) is Not Supported."
+    )
     subprocess.run("cls", shell=True)
     print(
-        f"{Fore.RED}ERROR: {Fore.WHITE}Your Current OS ({Fore.RED}{platform.system()}{Fore.WHITE}) Is Not Supported. Sh{Fore.BLUE}DW {Fore.WHITE}only Supports being run under Windows."
+        f"{Fore.RED}ERROR: {Fore.WHITE}Your Current OS ({Fore.RED}{platform.system()}{Fore.WHITE}) Is Not Supported. Sh{Fore.BLUE}DW {Fore.WHITE}only Supports being run under Windows.\n"
     )
-    print("Please press any key to exit.")
+    print("Please Press any Key to Exit.")
     # Pause via subprocess, msvcrt is windows-specific and using getch() will not work.
     subprocess.run("pause >nul 2>&1", shell=True)
     sys.exit(1)
 
 
-
 if not ctypes.windll.shell32.IsUserAnAdmin():
-    print(f"{Fore.RED}User Error; {Fore.WHITE}Sh{Fore.BLUE}DW{Fore.WHITE} was not executed with escelated privilleges.\n")
+    print(
+        f"{Fore.RED}User Error; {Fore.WHITE}Sh{Fore.BLUE}DW{Fore.WHITE} was not executed with escelated privilleges.\n"
+    )
     for i in range(3, 0, -1):
-        print(f"{Fore.LIGHTBLUE_EX}DEBUGGING; {Fore.WHITE}Attempting to auto-elevate. Elevating in {Fore.LIGHTGREEN_EX}{i}{Fore.WHITE}...", end='\r')
+        ctypes.windll.kernel32.SetConsoleTitleW(f"Elevating In {i}")
+        print(
+            f"{Fore.LIGHTBLUE_EX}DEBUGGING; {Fore.WHITE}Attempting to auto-elevate. Elevating in {Fore.LIGHTGREEN_EX}{i}{Fore.WHITE}...",
+            end="\r",
+        )
         sleep(1)
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, " ".join(sys.argv), None, 1
+    )
     sys.exit(0)
 
 
@@ -47,7 +57,9 @@ else:
     tries = 0
     while not check_internet_status():
         subprocess.run("cls", shell=True)
-        print(f"{Fore.RED}ERROR; Failed to resolve test host ({'https://duckduckgo.com'}).{Fore.WHITE}\n")
+        print(
+            f"{Fore.RED}ERROR; Failed to resolve test host ({'https://duckduckgo.com'}).{Fore.WHITE}\n"
+        )
         print(
             f"{Fore.GREEN}INFO; {Fore.WHITE}It is likely you have no Active Internet Connection established.\n"
         )
@@ -58,7 +70,7 @@ else:
             f"{Fore.LIGHTBLUE_EX}DEBUGGING; {Fore.WHITE}Attempts to Connect: {Fore.BLUE}{tries}{Fore.WHITE}\n"
         )
         ctypes.windll.kernel32.SetConsoleTitleW(
-            f"Failed Connection Check! Attempts to Connect: {tries}"
+            f"ERROR: Failed Connection Check! Attempts to Connect: {tries}"
         )
         tries += 1
         sleep(1.5)
